b54a92f5fad23382cf6b7f2c0b7c6bb2ea398dcf
clarkeand <superclarke909@gmail.com>
2023-06-01 16:48:02 -0700

    player seeding complete (EXCEPT IMG and POKED) - starting teams.

--- seed_database.py
+++ seed_database.py
@@ -8,8 +8,8 @@ import crud
 import model
 import server
 
-os.system("dropdb ratings")
-os.system('createdb ratings')
+os.system("dropdb favorites")
+os.system('createdb favorites')
 model.connect_to_db(server.app)
 model.db.create_all()
 
@@ -17,40 +17,27 @@ player_dict = players.get_active_players()
 team_dict = teams.get_teams()
 
 # Create Players
-for movie in movie_data:
-    title, overview, poster_path = (
-        movie["title"],
-        movie["overview"],
-        movie["poster_path"],
-    )
-    release_date = datetime.strptime(movie["release_date"], "%Y-%m-%d")
-
-    db_movie = crud.create_movie(title, overview, release_date, poster_path))
-
 for player in player_dict: 
+    # Get player name/id from the common player dictionary. 
     player_id  = player_dict[player]['id']
     player_name = player_dict[player]['full_name']
+    # We need complex dictionary to pull each players 3 digit team identifier. 
     player_info = commonplayerinfo.CommonPlayerInfo(f'{player_id}')
     player_info = player_info.get_normalized_dict()
     player_info = player_info.get('CommonPlayerInfo')[0]
     team_id = player_info['TEAM_ABBREVIATION']
+    # Set each POKED score to a value of 0 for now. 
+    poked_score = 0
+    # Set each image to Null for now. 
+    player_image = "Null"
+
+    #user crud create_a_player function to create a player to add to our DB. 
+    player = crud.create_player(player_id,player_name,team_id,poked_score,player_image)
+    print(player)
+    model.db.session.add(player)
+
+# Create Teams
+for team in team_dict: 
 
 
-
-
-for n in range(10):
-    email = f"user{n}@test.com"  # Voila! A unique email!
-    password = "test"
-
-    user = crud.create_user(email, password)
-    model.db.session.add(user)
-
-    for _ in range(10):
-        random_movie = choice(movies_in_db)
-        score = randint(1, 5)
-
-        rating = crud.create_rating(user, random_movie, score)
-        model.db.session.add(rating)
-
-model.db.session.add_all(movies_in_db)
 model.db.session.commit()
